4f3f107de6de52224674c0ef1b6df2b0b3eb8977
David Gleba <dgleba@gmail.com>
2018-08-25 23:12:31 -0400

    working on perms 2018-08-25b

--- myproject/settings.py
+++ myproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'yw9g^kul-y6-6zage+qk!2^eortdl6#6slbln5gxdjj87ilaxg'
+SECRET_KEY = '6=^a(b7il8(x$-@mjo2@480ty#w^bt^dx8qqy-e1i1ynxnxw9s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'poll2',
 )
 
 MIDDLEWARE_CLASSES = (
